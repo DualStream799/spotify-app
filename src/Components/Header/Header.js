7300312900825cf448c5bca50f3e24923cb81a60
Thalia Loiola <thalials@al.insper.edu.br>
2020-11-21 23:00:47 -0300

    change logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,3 @@
-// import React, { useContext } from 'react';
-// import { AppBar, Toolbar } from '@material-ui/core';
-// import Typography from '@material-ui/core/Typography';
-// import { Link } from 'react-router-dom';
-// import { Context } from '../../Context/AuthContext';
-// import SearchBar from '../../Components/SearchBar';
-// import './styles.css';
-// import history from '../../history';
-// import Logo from './logo'
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
@@ -16,26 +6,25 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Zoom from '@material-ui/core/Zoom';
-
+import logoLindo from '../../logoLindo.png'
 const useStyles = makeStyles((theme) => ({
     root: {
       position: 'fixed',
       bottom: theme.spacing(2),
       right: theme.spacing(2),
     },
+    appBar: {
+      height: 60,
+    }
   }));
   
   function ScrollTop(props) {
     const { children, window } = props;
     const classes = useStyles();
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
+
     const trigger = useScrollTrigger({
       target: window ? window() : undefined,
       disableHysteresis: true,
@@ -61,10 +50,6 @@ const useStyles = makeStyles((theme) => ({
     
     ScrollTop.propTypes = {
       children: PropTypes.element.isRequired,
-      /**
-       * Injected by the documentation to work in an iframe.
-       * You won't need it on your project.
-       */
       window: PropTypes.func,
     };
     
@@ -72,21 +57,13 @@ const useStyles = makeStyles((theme) => ({
       return (
         <React.Fragment>
           <CssBaseline />
-          <AppBar>
+          <AppBar style={{ background: '#000000' }}>
             <Toolbar>
-              <Typography variant="h6">Logo</Typography>
+              {/* <Typography variant="h6">Logo</Typography> */}
+              <img src={logoLindo} height="45" width="45" />
             </Toolbar>
           </AppBar>
           <Toolbar id="back-to-top-anchor" />
-          {/* <Container>
-            <Box my={2}>
-              {[...new Array(12)]
-                .map(
-                  () => ` `,
-                )
-                .join('\n')}
-            </Box>
-          </Container> */}
           <ScrollTop {...props}>
             <Fab color="secondary" size="small" aria-label="scroll back to top">
               <KeyboardArrowUpIcon />
